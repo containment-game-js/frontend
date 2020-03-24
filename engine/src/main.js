03a1b914071aca365cca2d43e82b2f5077fc8dd9
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-03-24 17:00:07 +0100

    :fire: Move code

--- engine/src/main.js
+++ engine/src/main.js
@@ -25,33 +25,3 @@ class Engine_ {
 }
 
 export { Engine }
-
-const engine = new Engine({
-  filter: (state, player) => ({ ...state }),
-  actions: {
-    guess(state, player, params) {},
-  },
-  state: init(),
-  players: [
-    {
-      id: '56142e68-5e47-4c8e-83dd-4451c08011ae',
-      team: 'red',
-      spy: true,
-    },
-    {
-      id: '8e87050f-ffa4-4b93-bf55-1bda8de98753',
-      team: 'blue',
-      spy: true,
-    },
-    {
-      id: '56142e68-5e47-4c8e-83dd-4451c08011ae',
-      team: 'red',
-      spy: false,
-    },
-    {
-      id: '8e87050f-ffa4-4b93-bf55-1bda8de98753',
-      team: 'blue',
-      spy: false,
-    },
-  ],
-})
