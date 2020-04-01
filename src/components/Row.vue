ecf8757c001ec9ba29a4ba0177cef6bdb296b98c
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-04-01 13:10:28 +0200

    :sparkles: App props to row

--- src/components/Row.vue
+++ src/components/Row.vue
@@ -1,11 +1,64 @@
 <template lang="html">
-  <div class="row">
+  <div
+    :class="{
+      row: true,
+      space,
+      grow,
+      'no-wrap': noWrap,
+      full,
+      [`align-${align}`]: true,
+    }"
+  >
     <slot name="default" />
   </div>
 </template>
 
+<script>
+export default {
+  props: {
+    space: Boolean,
+    grow: Boolean,
+    noWrap: Boolean,
+    full: Boolean,
+    align: {
+      type: String,
+      default: 'none',
+      validator(value) {
+        const values = ['none', 'center']
+        return values.includes(value)
+      },
+    },
+  },
+}
+</script>
+
 <style lang="css" scoped>
 .row {
   display: flex;
+  flex-wrap: wrap;
+}
+
+.space {
+  justify-content: space-between;
+}
+
+.grow > * {
+  flex: 1;
+}
+
+.no-wrap {
+  flex-wrap: nowrap;
+}
+
+.full {
+  height: 100%;
+}
+
+.align-none {
+  align-items: stretch;
+}
+
+.align-center {
+  align-items: center;
 }
 </style>
