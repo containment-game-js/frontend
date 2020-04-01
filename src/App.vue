49f2def4dc6f3dcc332b05cd6068f67d763bdbdf
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-04-01 13:11:25 +0200

    :lipstick: Add colors for light and dark schemes

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,22 @@
 </template>
 
 <style>
+:root {
+  --ternary: #112d4e;
+  --secondary: #3f72af;
+  --primary: #dbe2ef;
+  --background: #f9f7f7;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --ternary: #bbe1fa;
+    --secondary: #3282b8;
+    --primary: #0f4c75;
+    --background: #1b262c;
+  }
+}
+
 body {
   margin: 0;
 }
