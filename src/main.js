9f08dca8647b482286f61918991605e58f27c002
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-04-01 13:41:08 +0200

    :heavy_plus_sign: Add vuejs-logger to log things

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
+import VueLogger from 'vuejs-logger'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+const options = {
+  isEnabled: true,
+  logLevel: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+  showLogLevel: true,
+  showMethodName: true,
+  showConsoleColors: true,
+}
+
+Vue.use(VueLogger, options)
 
 new Vue({
   router,
