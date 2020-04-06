bf65e1512f6167a180095f61654911187934261c
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-04-06 17:02:42 +0200

    :recycle: Move input username into it’s component file

---
+++ src/views/Username/Card.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <card pad-y>
+    <card-header>{{ $t('home.title.username') }}</card-header>
+    <card-content>
+      <input
+        class="full"
+        @keydown.space.prevent
+        @input="$store.commit('updateName', $event.target.value)"
+        :value="$store.state.name"
+        :placeholder="$t('home.input.username')"
+      />
+    </card-content>
+  </card>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+import CardHeader from '@/components/Card/Header.vue'
+import CardContent from '@/components/Card/Content.vue'
+
+export default {
+  components: {
+    Card,
+    CardHeader,
+    CardContent,
+  },
+}
+</script>
