1d19a07d22aa27d6a31997008b3c4ea58b8a9c16
Guillaume Hivert <hivert.is.coming@gmail.com>
2020-05-06 14:01:06 +0200

    :wrench: Socket and backend URL

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,9 +1,17 @@
 const connectionURL = () => {
   if (process.env.NODE_ENV === 'development') {
-    return 'http://localhost:3030'
+    return 'http://localhost:5000'
   } else {
     return 'https://api.containment-ga.me'
   }
 }
 
-export { connectionURL }
+const socketURL = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return 'http://localhost:3030'
+  } else {
+    return 'https://socket.containment-ga.me'
+  }
+}
+
+export { connectionURL, socketURL }
